e2d27a335502a71ae14df4c96b4746ed8a81a914
alistkov <antonlistkov@gmail.com>
2023-02-11 15:36:59 +0300

    video 142

--- src/components/Layout/Header/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from './CartIcon';
-
+import CartContext from '../../../../store/cart-contex';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = ({ onClick }) => {
+  const context = useContext(CartContext);
+
+  const cartMealsCount = context.items.reduce(
+    (acc, count) => acc + count.amount,
+    0
+  );
+
   return (
     <button className={classes.button} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{cartMealsCount}</span>
     </button>
   );
 };
